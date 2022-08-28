5b8f0eee9ed7ae221c7463a734574b0c5646bdc3
pyt3h <99658549+pyt3h@users.noreply.github.com>
2022-08-28 21:33:35 +0700

    Update views.py

--- Day-26/post_method/app/views.py
+++ Day-26/post_method/app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.db import connection
+
 @csrf_exempt
 @require_http_methods(['POST']) 
 def test_post(request):
