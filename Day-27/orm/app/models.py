7f586223c433ea8697c8136e93014684ef8e9671
khanhpv195 <khanhpoly@gmail.com>
2022-08-31 20:29:43 +0700

    Update

---
+++ Day-27/orm/app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20, unique=True)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    parent = models.ForeignKey('Category', on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    qty = models.IntegerField()
+    price_unit = models.IntegerField()
+    order_date = models.DateTimeField()
+
