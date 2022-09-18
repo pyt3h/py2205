e531c4137e1eb26e16859e33242c8e646f167fa7
khanhpv195 <khanhpoly@gmail.com>
2022-09-18 20:27:40 +0700

    Update

--- Day-31/supermarket/app/models.py
+++ Day-31/supermarket/app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20, unique=True)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    parent = models.ForeignKey('Category', blank=True, null=True, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=100, unique=True)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    order_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.order_number
+
+class Order_item(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    qty = models.IntegerField()
+    price_unit = models.IntegerField()
+
