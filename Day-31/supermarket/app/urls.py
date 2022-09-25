af6d3fc19d78ee839d6b50b515a6d9b77171d681
khanhpv195 <khanhpoly@gmail.com>
2022-09-25 20:00:03 +0700

    Update

--- Day-31/supermarket/app/urls.py
+++ Day-31/supermarket/app/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('search-customer', search_customer),
     path('search-product', search_product),
+    path('purchase', purchase),
     #path('hello', hello)
 ]
